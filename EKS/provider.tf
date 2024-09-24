65e752d8510a4f8ecdb6146f8ba531d52c2e112a
veerababu016 <151728680+veerababu016@users.noreply.github.com>
2024-09-24 14:33:04 +0530

    Update provider.tf

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -1,16 +1,16 @@
 locals {
-  region = "ap-south-1"
+  region = "ap-southeast-1"
   name   = "amonkincloud-cluster"
-  vpc_cidr = "10.123.0.0/16"
-  azs      = ["ap-south-1a", "ap-south-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  vpc_cidr = "172.31.0.0/16"
+  azs      = ["ap-southeast-1a", "ap-southeast-1b"]
+  public_subnets  = ["172.31.32.0/20"]
+  private_subnets = ["172.31.16.0/20"]
+  #intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
   tags = {
     Example = local.name
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-southeast-1"
 }
